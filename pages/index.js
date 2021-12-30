d083168c323558cdf1c335c81e884caff5a6592f
Adion52330 <85357798+Adion52330@users.noreply.github.com>
2021-12-30 11:05:37 +0530

    Added meta tag

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,12 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Virary - Best book managing app</title>
         <link rel="icon" href="/virary2.png" />
+        <meta
+          name="description"
+          content="Bookworm like me? Want to take note of each book you read for free? Virary is the answer. Store all your books into your account anywhere and anytime. Access them anytime, anywhere. Virary is the best virtual library. Store them easily via ISBN or use custom add."
+        />
       </Head>
 
       <div>
