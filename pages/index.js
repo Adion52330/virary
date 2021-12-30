1e1eb8aafc2af61d5a17b9383d76e686ae1b75fa
Aditya <85357798+Adion52330@users.noreply.github.com>
2021-12-30 17:31:03 +0530

    added google-site-verification

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
           content="Bookworm like me? Want to take note of each book you read for free? Virary is the answer. Store all your books into your account anywhere and anytime. Access them anytime, anywhere. Virary is the best virtual library. Store them easily via ISBN or use custom add."
         />
     <meta name="robots" content="noindex,nofollow" />
+    <meta name="google-site-verification" content="DbJ8Exa44O0xv8HrxBW_lMhGhl2qCd7WDs5ZD_UOHrk" />
       </Head>
 
       <div>
